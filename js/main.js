53448ea1e1b5af8b3ff783d24ec7129708d170b9
Stanislau <omgnoooob@gmail.com>
2020-12-23 01:17:42 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.menu__btn').on('click', function () {
+        $('.header__list').toggleClass('header__list_active')
+    })
+    $('.menu__btn').on('click', function () {
+        $('.center-wrapper').toggleClass('center-wrapper_hidden')
+    })
+    $('.susan__about').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
